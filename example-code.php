0ddcc6ae67eec26a2e23d7b673d07ffc31d91c08
Jasper Frumau <jasper@imagewize.com>
2017-03-10 10:31:14 +0300

    B4 Fix

--- example-code.php
+++ example-code.php
@@ -1,25 +1,25 @@
 <div class="container">
 	<div class="row">
-		<!-- Carousel -->
-    	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-			<!-- Indicators -->
+    <!-- Carousel -->
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width:100%;">
+      <!-- Indicators -->
       <?php $the_slides_query_ol = new WP_Query('post_type=slider&showposts=3'); ?>
           <?php if ( $the_slides_query_ol->have_posts() ) : ?>
-			<ol class="carousel-indicators">
+      <ol class="carousel-indicators">
         <?php while ( $the_slides_query_ol->have_posts() ) : $the_slides_query_ol->the_post(); ?>
           <li data-target="#carousel-example-generic" data-slide-to="<?php echo $the_slides_query_ol->current_post ?>" class="<?php if( $the_slides_query_ol->current_post == 0 ) echo 'active' ?>"></li>
           <?php wp_reset_postdata(); ?>
           <?php endwhile; ?>
-			</ol>
+      </ol>
       <?php endif; ?>
-			<!-- Wrapper for slides -->
+      <!-- Wrapper for slides -->
       <?php $the_slides_query = new WP_Query('post_type=slider&showposts=3'); ?>
           <?php if ( $the_slides_query->have_posts() ) : ?>
-			<div class="carousel-inner">
+      <div class="carousel-inner" role="listbox">
         <?php while ( $the_slides_query->have_posts() ) : $the_slides_query->the_post(); ?>
-			    <div class="item <?php if( $the_slides_query->current_post == 0 ) echo 'active' ?>">
+          <div class="carousel-item <?php if( $the_slides_query->current_post == 0 ) echo 'active' ?>">
             <?php if (has_post_thumbnail()): ?>
-                <?php the_post_thumbnail('', array('class' => 'img-responsive')); ?>
+                <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full', 'title' => 'Feature image']); //the_post_thumbnail( 'full' ); ?>
                 <?php endif; ?>
                   <!-- Static Header -->
                     <div class="header-text hidden-xs">
@@ -31,25 +31,25 @@
                             </h2>
                             <br>
                             <h3>
-                            	<?php if (get_the_content()) {?>
+                              <?php if (get_the_content()) {?>
                               <span><?php the_content(); ?></span>
                               <?php } ?>
                             </h3>
                         </div> <!-- / text center -->
                     </div><!-- /header-text -->
-			    </div><!-- end item -->
+          </div><!-- end item -->
           <?php wp_reset_postdata(); ?>
             <?php endwhile; ?>
-			</div> <!-- end carousel inner -->
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-		    	<span class="glyphicon glyphicon-chevron-left"></span>
-			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-		    	<span class="glyphicon glyphicon-chevron-right"></span>
-			</a>
-		</div><!-- /carousel -->
-	</div><!-- end row -->
+      </div> <!-- end carousel inner -->
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div><!-- /carousel -->
+  </div><!-- end row -->
   <?php endif; ?>
 </div> <!-- end container -->
 <?php the_content(); ?>
