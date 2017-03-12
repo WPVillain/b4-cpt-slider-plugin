3c36f8b5b3f797b2bc2f91c0cd8faf9d8ed00a58
Jasper Frumau <jasper@imagewize.com>
2017-03-12 12:02:02 +0300

    Create cmb2-functions.php

---
+++ cmb2-functions.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+* http://willthemoor.github.io/cmb2-metabox-generator/
+* CMB2 Custom fields to load a header, sub header text and CTA
+*
+*/
+
+add_action( 'cmb2_init', 'filathlos_add_carousel_metabox' );
+function filathlos_add_carousel_metabox() {
+
+	$prefix = '_filathlos_';
+
+	$cmb = new_cmb2_box( array(
+		'id'           => $prefix . 'carousel_fields',
+		'title'        => __( 'Carousel Fields', 'filathlos' ),
+		'object_types' => array( 'slider' ),
+		'context'      => 'normal',
+		'priority'     => 'high',
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Carousel Header', 'filathlos' ),
+		'id' => $prefix . 'carousel_title',
+		'type' => 'text',
+		'default' => 'title',
+		'desc' => __( 'Carousel Header', 'filathlos' ),
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Carousel Sub Header', 'filathlos' ),
+		'id' => $prefix . 'carousel_sub_header',
+		'type' => 'text',
+		'default' => 'sub header',
+		'desc' => __( 'Carousel Sub Header', 'filathlos' ),
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'Call to Action Button', 'filathlos' ),
+		'id' => $prefix . 'carousel_cta',
+		'type' => 'text',
+		'default' => 'cta',
+		'desc' => __( 'Call to Action Button', 'filathlos' ),
+	) );
+
+	$cmb->add_field( array(
+		'name' => __( 'The Call To Action url or link', 'filathlos' ),
+		'id' => $prefix . 'carousel_cta_url',
+		'type' => 'text',
+		'default' => 'https:/imagewize.com',
+		'desc' => __( 'The Call To Action url or link', 'filathlos' ),
+	) );
+
+}
